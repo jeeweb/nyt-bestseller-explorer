ef6da77335084422604d50bdf268865f855f161b
jeeweb <52396673+jeeweb@users.noreply.github.com>
2024-03-30 23:41:53 +0900

    setup

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import "../styles/global.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "NYT Best Seller Explorer",
+  description:
+    "The New York Times Best Sellers are up-to-date and authoritative lists of the most popular books in the United States, based on sales in the past week, including fiction, non-fiction, paperbacks, children’s books, audiobooks, graphic books and more.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
