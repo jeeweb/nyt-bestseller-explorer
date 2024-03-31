7f369814a4cd10e3a9c944d604ff22da14cb3b42
jeeweb <52396673+jeeweb@users.noreply.github.com>
2024-03-31 22:40:54 +0900

    feat: home - book categories

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,34 @@
-export default function HomePage() {
+import Link from "next/link";
+import styles from "../styles/home.module.css";
+import { API_URL } from "./constants";
+
+async function getCategories() {
+  return fetch(`${API_URL}/lists`).then((response) => response.json());
+}
+
+export default async function HomePage() {
+  const categories = await getCategories();
+  //console.log(categories);
   return (
-    <div>
-      <h1>The New York Times Best Seller Explorer</h1>
+    <div className={styles.container}>
+      <h1 className={styles.pageTitle}>
+        The New York Times
+        <br />
+        Best Seller Explorer
+      </h1>
+      <section>
+        <ul>
+          {categories.results.map((category) => {
+            return (
+              <li key={category.list_name_encoded}>
+                <Link href={`/list/${category.list_name_encoded}`}>
+                  {category.display_name}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
     </div>
   );
 }
