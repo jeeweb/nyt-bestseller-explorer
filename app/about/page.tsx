4e60ea5d79ed1de0063c3ba2ed5d549429da6f66
jeeweb <52396673+jeeweb@users.noreply.github.com>
2024-03-31 22:12:23 +0900

    feat: about 페이지 추가

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,15 @@
+import styles from "../../styles/about.module.css";
+
 export default function About() {
-  return <div>About</div>;
+  return (
+    <>
+      <h2 className={styles.pageTitle}>About Us</h2>
+      <p>
+        Welcome to the official explorer for The New York Times Best Seller list
+        explorer.
+        <br />
+        We hope you enjoy your stay!
+      </p>
+    </>
+  );
 }
